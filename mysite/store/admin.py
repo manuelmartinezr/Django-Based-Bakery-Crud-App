aaea7b8fc59b6c639c8101b425f2ed870424ff82
Manuel Martínez <manuelmartinezr18@gmail.com>
2024-06-03 10:58:07 -0500

    created models; connected template display to database

--- mysite/store/admin.py
+++ mysite/store/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
+admin.site.register(Customer)
+admin.site.register(DeliveryPerson)
+admin.site.register(DeliverySchedules)
+admin.site.register(DeliveryVehicle)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Administrator)
 # Register your models here.
--- mysite/store/models.py
+++ mysite/store/models.py
@@ -1,3 +1,88 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.first_name
+
+class DeliveryPerson(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+class Administrator(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    first_name = models.CharField(max_length=100)
+
+class Order(models.Model):
+    date = models.DateField()
+    entry_time = models.TimeField()
+    delivery_date = models.DateField()
+    delivery_time = models.TimeField()
+    complete = models.BooleanField(default=False, null=True, blank=False)
+    delivery_person = models.ForeignKey(DeliveryPerson, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+    
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
+class DeliverySchedules(models.Model):
+    delivery_person = models.ForeignKey(DeliveryPerson, on_delete=models.CASCADE)
+    schedule = models.JSONField
+
+class DeliveryVehicle(models.Model):
+    delivery_person = models.ForeignKey(DeliveryPerson, on_delete=models.CASCADE)
+    VEHICLE_CHOICES = [
+        ('car', 'Car'),
+        ('motorcycle', 'Motorcycle'),
+        ('bicycle', 'Bicycle'),
+    ]
+    vehicle = models.CharField(max_length=10, choices=VEHICLE_CHOICES)
+    license_number = models.CharField(max_length=50)
+    expiration_date = models.DateField()
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    product_image = models.ImageField(null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+    @property
+    def imageURL(self):
+        try:
+            url = self.product_image.url
+        except:
+            url = ''
+        return url
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
